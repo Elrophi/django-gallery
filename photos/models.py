b29d045ef94aa4a03347bba5c6dfc98e1a6a12e5
el <elrophilskwaila@gmail.com>
2021-05-15 19:53:49 +0300

    Added models and methods

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+import datetime as dt
+from django.db.models.aggregates import Max
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.location_name
+
+    @classmethod
+    def get_location(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def save_location(self):
+        return self.save()
+
+    def delete_location(self):
+        return self.delete()
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.category_name
+
+    def save_category(self):
+        return self.save()
+
+    def delete_category(self):
+        return self.delete()
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length=100)
+    image_description = models.TextField()
+    author = models.CharField(max_length=60, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    image_location = models.ForeignKey(Location)
+    image_category = models.ForeignKey(Category)
+
