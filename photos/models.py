931339ffeb5fdc669b77fd5352e266326784b020
el <elrophilskwaila@gmail.com>
2021-05-15 21:15:13 +0300

    Added image methods

--- photos/models.py
+++ photos/models.py
@@ -47,3 +47,32 @@ class Image(models.Model):
     image_location = models.ForeignKey(Location)
     image_category = models.ForeignKey(Category)
 
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        return self.save()
+
+    def delet_image(self):
+        return self.delete()
+
+    @classmethod
+    def update_image(cls, id, value):
+         cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_bby_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_image(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+
