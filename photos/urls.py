75281137f85ee1f4f09449e4ba2bb899980d162a
el <elrophilskwaila@gmail.com>
2021-05-17 15:47:48 +0300

    copy link

--- photos/urls.py
+++ photos/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/',views.search, name='search'),
-    path('location/', views.image_location, name='location'),
+    path('location/(?P<location>\w+)', views.image_location, name='location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
